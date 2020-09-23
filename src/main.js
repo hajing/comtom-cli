901768356ad017e6d16ed2b89554e196c37da736
hejing <hejing@comtom.cn>
2020-09-23 09:34:10 +0800

    新增插件，并完善次脚手架

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+/*
+引入常用工具类库
+1. UI组件库：iview；
+2. 用于处理对象、数组、数学运算的工具：lodash；
+3. 用于处理时间格式、时间运算等的工具：dayjs；
+4. 自适应转换工具：postcss-px-to-viewport；
+5. 数据图标库：echarts。
+*/
+import ViewUI from "view-design";
+import "view-design/dist/styles/iview.css";
+import lodash from "lodash";
+import dayjs from "dayjs";
+import echarts from "echarts";
+
+/*
+引入常用工具类库结束
+*/
+
+Vue.use(ViewUI);
+Vue.prototype.$lodash = lodash;
+Vue.prototype.$dayjs = dayjs;
+Vue.prototype.$echarts = echarts;
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
